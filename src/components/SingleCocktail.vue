8293e30d18e8ad51a75967015b77d24d977ab125
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-09-05 12:34:44 +0200

    added SingleCocktail component to components folder

---
+++ src/components/SingleCocktail.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    @use '../styles/general.scss' as *;
+    
+</style>

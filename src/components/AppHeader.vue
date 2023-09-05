9ae22eb5dc182190d819fe4007ed9d0210d25a0b
Ericson-Manalo <ericmana98@gmail.com>
2023-09-05 15:59:06 +0200

    created a v-for in li and created an array links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-        aaaaaaaaaaaaaa
-    </div>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active" v-for="link in links">
+                        <router-link :to="{name: link.route}">
+                            {{ link.name }}
+                        </router-link>
+                    </li>
+                </ul>
+        </nav>
+    </header>
 </template>
 <script>
 export default {
@@ -9,7 +17,20 @@ export default {
 
     data(){
         return{
-
+            links : [
+                {
+                    route: 'cocktails',
+                    name: 'Cocktails'
+                },
+                {
+                    route: 'aboutUs',
+                    name: 'AboutUs'
+                },
+                {
+                    route: 'contacts',
+                    name: 'Contact'
+                }
+            ]
         }
     },
     
